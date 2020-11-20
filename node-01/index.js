41b2b421b215c9feb363a7494365f35204e92e1e
Maria Agustina Garcia <agus.garcia17@gmail.com>
2020-11-20 22:06:47 +0100

    primer commit

---
+++ node-01/index.js
@@ -0,0 +1,26 @@
+// Crear un archivo de javascript que tenga:
+//  - una linea donde hagan console.log de su nombre y apellido
+//  - console.log  del resultado de una suma
+//  - declarar un array con sus peliculas, series o libros favoritos
+//  - generar un objeto con esa información
+//  - hacer console.log del objeto completo
+//  - hacer console.log de sus propiedades
+
+
+console.log("Agustina Garcia")
+console.log(45+54)
+
+const arr = ["the big bang theory", "breaking bad", "gossip girl", "brooklyn 99"]
+
+const series = {
+    serie01: "the big bang theory",
+    serie02: "breaking bad",
+    serie03: "gossip girl",
+    serie04: "brooklyn 99",
+}
+
+console.log(series)
+console.log(series.serie01)
+console.log(series.serie02)
+console.log(series.serie03)
+console.log(series.serie04)
