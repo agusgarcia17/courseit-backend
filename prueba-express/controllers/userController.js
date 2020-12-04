c62d7228b46e557ba55bc959ba7931ce1b4ba451
Maria Agustina Garcia <agus.garcia17@gmail.com>
2020-12-04 23:11:08 +0100

    controllers

--- prueba-express/controllers/userController.js
+++ prueba-express/controllers/userController.js
@@ -2,6 +2,22 @@ class UserController {
     getUser(req,res) {
         res.send(`Hola ${req.params.name}`)
     }
+
+    multiplyNumber(req,res) {
+        res.send(`El numero multiplicado por dos es: ${req.params.number * 2} `);
+    }
+
+    checkPalindrome(req,res){
+        const word = req.params.palabra
+
+        const palindromo = word.split("").reverse().join("")
+      
+          if(palindromo === word) {
+            res.send(`La frase ${word} es un palíndromo ya que al revés se lee: ${palindromo}.`);
+          } else {
+            res.send(`La frase ${word} NO es un palíndromo ya que al revés se lee: ${palindromo}`);
+          }
+    }
 }
 
 module.exports = UserController
--- prueba-express/routes/index.js
+++ prueba-express/routes/index.js
@@ -14,20 +14,11 @@ router.get('/test/:name', function(req, res, next) {
 });
 
 router.get('/numeros/:number', function(req, res, next) {
-  res.send(`El numero multiplicado por dos es: ${req.params.number * 2} `);
+  UserInstance.multiplyNumber(req, res)
 });
 
 router.get('/palindromo/:palabra', function(req, res, next) {
-
-  const word = req.params.palabra
-
-  const palindromo = word.split("").reverse().join("")
-
-    if(palindromo === word) {
-      res.send(`La frase ${word} es un palíndromo ya que al revés se lee: ${palindromo}.`);
-    } else {
-      res.send(`La frase ${word} NO es un palíndromo ya que al revés se lee: ${palindromo}`);
-    }
+  UserInstance.checkPalindrome(req, res)
 });
 
 
