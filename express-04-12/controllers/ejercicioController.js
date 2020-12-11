57cddadc6377e531726ce1dc0ad883f760de85de
Maria Agustina Garcia <agus.garcia17@gmail.com>
2020-12-11 21:18:03 +0100

    corrijo ejercicio

--- express-04-12/controllers/ejercicioController.js
+++ express-04-12/controllers/ejercicioController.js
@@ -20,47 +20,42 @@ class AxiosController{
         // res.send(`Existen ${primes.length} numeros primos hasta el 150 y son ${primes}`)
 
         const numerosData = {
-            "cantidad" :  `${primes.length}`,
+            "cantidad" : `${primes.length}`,
             "numeros" : `${primes}`
         }
 
         res.json(numerosData)
 }
-
-async primosPokemon(req, res){
-    const data = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=150`)
-    const results = data.data.results
-    
-    let primes = []
-    for (var x = 1; x <= 150; x++) {
-        
-        var notPrime = false;
-        for (var i = 2; i <= x; i++) {
-            if (x%i===0 && i!==x) {
-                notPrime = true;
+    checkPrime(num) {
+        let isPrime = true;
+        for (let i = 2; i <= 150; i++) {
+            if (num%i===0 && i!==num) {
+                isPrime = false;
             }
-        }
-        if (notPrime === false) {
-            primes.push(x);
-        }
-    }
+        } 
+        return isPrime
+    } 
 
-    // let pokemon = []
-
-    // primes.forEach(num => {
-    //     pokemon.push({
-    //         "primeNumber": `${num}`,
-    //         "pokemonName":`${results[num].name}`
-    //     })
-
-    // })
-
-
-    let pokemon = primes.map(num => {
-                return {
-                    "primeNumber": `${num}`,
-                    "pokemonName":`${results[num].name}`
-                }
+    async primosPokemon(req, res){
+    
+        const data = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=150`)
+        const results = data.data.results
+        
+ 
+    let pokemon = results
+        .map((num, i) => { 
+        const id = i+1
+        if(this.checkPrime(id)) {
+            return {
+                "primeNumber": `${id}`,
+                "pokemonName":`${results[i].name}`
+            }
+        } 
+    }) 
+        .filter((item)=>{
+            if(item !== null){
+                return item
+            }
     })
 
     res.json(pokemon)
